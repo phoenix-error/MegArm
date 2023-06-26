7ec3a683b89dc87fd360a380658dba8323c65794
markus1234D <48254068+markus1234D@users.noreply.github.com>
2023-06-26 15:27:49 +0200

    Update MotorControl.java

--- MotorControl.java
+++ MotorControl.java
@@ -35,8 +35,17 @@ public class MotorControl {
 		double baseAngle = Math.atan2(y, x)*(180/Math.PI);
 		double l = Math.sqrt(x*x + y*y);
 		double elbowAngle = - Math.acos((l*l + z*z - lowerArmLen*lowerArmLen - upperArmLen*upperArmLen)/ (2*upperArmLen*lowerArmLen))*(180/Math.PI);
+//		
+//		double shoulderAngle = Math.atan2(z, l)*(180/Math.PI) + Math.asin(((upperArmLen*Math.sin(elbowAngle))) / (lowerArmLen + upperArmLen*Math.cos(elbowAngle)))*(180/Math.PI);
 		
-		double shoulderAngle = Math.atan2(z, l)*(180/Math.PI) + Math.atan2(((upperArmLen*Math.sin(elbowAngle))), lowerArmLen + upperArmLen*Math.cos(elbowAngle))*(180/Math.PI);
+		double gamma = Math.atan2(z, l)*(180/Math.PI);
+		double r = Math.sqrt(l*l + z*z);
+		
+		double an= Math.cos(-elbowAngle) * upperArmLen;
+		double geg= Math.sin(-elbowAngle) * upperArmLen;		
+//		double beta = Math.atan2(geg, lowerArmLen + an)*(180./Math.PI);
+		double beta = Math.acos((lowerArmLen+an)/r)*(180/Math.PI);
+		double shoulderAngle = beta + gamma;
 		
 		base.rotate((int)Math.round(baseAngle));
 		elbow.rotate((int)Math.round(elbowAngle));
